6faf010370443f1fcc3fe332ed72f0c186f22d0b
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-16 21:00:23 +0530

    commit UC4

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -2,11 +2,14 @@ package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.model.User;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
@@ -25,5 +28,12 @@ public String sayHello(@RequestParam (value ="name")String name)
 public String satHelloParam(@PathVariable String name)
 {
 	return "Hello"+name  +"!";
+}
+
+@PostMapping("/post")
+public String sayHello(@RequestBody User user)
+{
+	return "Hello" +" "+ user.getFirstName() + " " + user.getLastName() +
+			" " + "From  Bridgelabz"; 
 }
 }
